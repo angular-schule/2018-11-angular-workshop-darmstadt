3a9a3645cdf776ae165cb7ffd06616d0ce7321a9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-11-15 09:56:18 +0100

    my first behaviour driven test

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
+  let rateUpWasCalled;
+  const ratingMock = {
+    rateUp: () => { rateUpWasCalled = true; },
+    rateUpAllowed: () => { },
+    rateDownAllowed: () => { }
+  };
+
   beforeEach(async(() => {
+    rateUpWasCalled = false;
     TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [
+        BookComponent
+      ],
+      providers: [{
+        provide: BookRatingService,
+        useValue: ratingMock
+      }]
     })
     .compileComponents();
   }));
@@ -16,10 +31,21 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = {
+      isbn: '00',
+      title: 'asdasd',
+      description: 'adsasdad',
+      rating: 1
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should forward the rateUp call to the book rating service', () => {
+    component.rateUp();
+    expect(rateUpWasCalled).toBe(true);
   });
 });
